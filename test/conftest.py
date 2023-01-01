66ea9e4aa8dec5eea8a9cbb96bc4e70e5b00b8a6
sjrojanooo <sjrojano@gmail.com>
2023-01-01 10:54:07 -0700

    edit

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 """
-This conftest file is used to make fixtures accessible throughout the entire directory. Instead
-of importing them into every test file, we can declare our spark session here and use it throughout 
+This conftest file contains a scope that is used to make fixtures 
+accessible throughout the entire directory. Instead of importing 
+them into every test file, we can declare our spark session here and use it throughout 
 the projects directory. When we run the docker-compose up test command our test function will request
 the spark_session fixture, and pytest will find it and capture what it returns. In our case when we 
 we call spark_session, pytest will return the SparkSession defined in the function. 
