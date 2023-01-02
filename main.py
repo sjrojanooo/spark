39fcb609ac75ca25d883e115663636e3563b4dde
sjrojanooo <sjrojano@gmail.com>
2023-01-02 16:00:46 -0700

    day difference from when adidas sales data start for a retailer.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
 from etl import adidas_transformations, file_transformations
+import re
 import os
 
 def main():
@@ -13,11 +14,13 @@ def main():
     adidas_zip_file = file_transformations.extract_zipfile(project_dir)
     target_file = file_transformations.extract_and_return_target_file(adidas_zip_file)
     file_transformations.move_and_rename_file(target_file)
+    # read csv and generate dataframe
     adidas_sales = spark.read.csv('./data/sales/adidas_us_retail_sales_data.csv', sep=',', header=True)
-    adidas_sales.show(Truncate=False)
-    # adidas_df = adidas_transformations.transform_datetime(adidas_df)
-    # adidas_df.cache()
-    # adidas_df.printSchema()
+    adidas_sales = adidas_transformations.transform_columns(adidas_sales)
+    adidas_sales = adidas_transformations.transform_datetime(adidas_sales)
+    adidas_sales = adidas_transformations.min_max_and_datediff(adidas_sales)
+    adidas_sales.show(truncate=False)
+    
 
 
 if __name__ == '__main__':
